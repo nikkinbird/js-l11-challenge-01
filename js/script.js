58a22fb35a9fb6d573a7d719658603ff94fabaee
nikkinbird <nikki.bollman@gmail.com>
2021-06-13 12:43:38 -0500

    Write function to select a random image

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getData = async function () {
+const getImages = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getData();
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    console.log(randomImage);
+}
+
+getImages();
+
